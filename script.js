12685802322e0c85382c421ebf4015d6d95f2a3f
Marine Darbinyan <darbimar@mail.ru>
2022-09-21 15:59:04 +0700

    Add JS dots

--- script.js
+++ script.js
@@ -36,3 +36,38 @@ function showSladerArrows(n) {
 }
 
 showSladerArrows(0);
+
+
+// Слайдер с точками
+
+const sliderDots = document.querySelector('.slider-dots'),
+    slidesDots = sliderDots.querySelectorAll('.slider-dots__item'),
+    wrapperDots = sliderDots.querySelector('.slider-dots__nav');
+
+console.log(sliderDots);
+
+const dots = [];
+
+for (let i = 0; i < slidesDots.length; i++) {
+    const dot = document.createElement('button');
+    dot.dataset.slideTo = i;
+    dot.classList.add('slider-dots__nav-item');
+    if (i == 0) {
+        dot.classList.add('slider-dots__nav-item--active');
+    }
+    if (i != 0) {
+        slidesDots[i].style.display = 'none';
+    }
+    dot.addEventListener('click', showSlideDots);
+    wrapperDots.append(dot);
+    dots.push(dot);
+}
+
+function showSlideDots(e) {
+    const slideTo = e.target.dataset.slideTo;
+    slidesDots.forEach(item => item.style.display = 'none');
+    slidesDots[slideTo].style.display = 'block';
+    dots.forEach(dot => dot.classList.remove('slider-dots__nav-item--active'));
+    e.target.classList.add('slider-dots__nav-item--active');
+}
+
